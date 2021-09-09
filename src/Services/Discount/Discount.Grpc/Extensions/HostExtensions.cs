1b1675f24897ab28cb7712ecf3e9ccee801cad7c
Matheus Otoni Amorim Silva <matheus.amorim@havan.com.br>
2021-09-09 13:58:48 -0300

    Log information

--- src/Services/Discount/Discount.Grpc/Extensions/HostExtensions.cs
+++ src/Services/Discount/Discount.Grpc/Extensions/HostExtensions.cs
@@ -36,18 +36,22 @@ namespace Discount.Grpc.Extensions
 
                     command.CommandText = "DROP TABLE IF EXISTS Coupon";
                     command.ExecuteNonQuery();
+                    logger.LogInformation("HostExtensions -> Drop table if exists");
 
                     command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, 
                                                                 ProductName VARCHAR(24) NOT NULL,
                                                                 Description TEXT,
                                                                 Amount INT)";
                     command.ExecuteNonQuery();
+                    logger.LogInformation("HostExtensions -> Create table");
 
                     command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
                     command.ExecuteNonQuery();
 
                     command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
                     command.ExecuteNonQuery();
+
+                    logger.LogInformation("HostExtensions -> Insert contents");
 
                     logger.LogInformation("HostExtensions -> Migrated postresql database.");
 
