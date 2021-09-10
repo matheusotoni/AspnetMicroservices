78da746a4590ff78e88f77cf4bd064ee576f8471
Matheus Otoni Amorim Silva <matheus.amorim@havan.com.br>
2021-09-10 13:11:38 -0300

    Removing Unused Dependencies

--- src/Services/Discount/Discount.API/Controllers/DiscountController.cs
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -2,8 +2,6 @@
 using Discount.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
