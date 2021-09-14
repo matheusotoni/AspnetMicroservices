8e48a9a17551406ec053a6bf6d81cbe22d1d5ade
Matheus Otoni Amorim Silva <matheus.amorim@havan.com.br>
2021-09-14 14:37:12 -0300

    Send email

--- src/Services/Ordering/Ordering.Infrastructure/Mail/EmailService.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Mail/EmailService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Ordering.Application.Contracts.Infrastructure;
 using Ordering.Application.Models;
 using SendGrid;
@@ -13,10 +14,10 @@ namespace Ordering.Infrastructure.Mail
         public EmailSettings _emailSettings { get; }
         public ILogger<EmailService> _logger { get; }
 
-        public EmailService(EmailSettings emailSettings, ILogger<EmailService> logger)
+        public EmailService(IOptions<EmailSettings> emailSettings, ILogger<EmailService> logger)
         {
-            _emailSettings = emailSettings ?? throw new ArgumentNullException(nameof(emailSettings));
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _emailSettings = emailSettings.Value;
+            _logger = logger;
         }
 
         public async Task<bool> SendEmail(Email email)
