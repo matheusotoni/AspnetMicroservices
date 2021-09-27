9b6b35e438491fd3701566effadca8f899ff777b
Matheus Otoni Amorim Silva <matheus.amorim@havan.com.br>
2021-09-27 16:48:06 -0300

    Create Shopping aggregator, docker configurations, services, extensions, controller, and update postman collection

---
+++ src/ApiGateways/Shopping.Agregator/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Shopping.Agregator.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+                throw new ApplicationException($"HttpClientExtensions -> Something went wrong calling the API: {response.ReasonPhrase}");
+
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
