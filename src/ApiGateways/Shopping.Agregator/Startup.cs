5a86121593ef89223fd1706a3baf9d7772474325
Matheus Otoni Amorim Silva <matheus.amorim@havan.com.br>
2021-10-15 11:01:34 -0300

    Apply Circuit Breaker Pattern with Polly policies on HttpClientFactory

--- src/ApiGateways/Shopping.Agregator/Startup.cs
+++ src/ApiGateways/Shopping.Agregator/Startup.cs
@@ -33,7 +33,8 @@ namespace Shopping.Agregator
             services.AddHttpClient<IBasketService, BasketService>(c =>
                 c.BaseAddress = new Uri(Configuration["ApiSettings:BasketUrl"]))
                 .AddHttpMessageHandler<LoggingDelegatingHandler>()
-                .AddTransientHttpErrorPolicy(policy => policy.WaitAndRetryAsync(3, _ => TimeSpan.FromSeconds(2)));
+                .AddTransientHttpErrorPolicy(policy => policy.WaitAndRetryAsync(3, _ => TimeSpan.FromSeconds(2)))
+                .AddTransientHttpErrorPolicy(policy => policy.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
 
             services.AddHttpClient<IOrderService, OrderService>(c =>
                 c.BaseAddress = new Uri(Configuration["ApiSettings:OrderingUrl"]))
