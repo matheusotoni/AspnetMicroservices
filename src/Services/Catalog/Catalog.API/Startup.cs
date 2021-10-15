b4761421f0736752b68b96aa0fc3e90d793dc8a3
Matheus Otoni Amorim Silva <matheus.amorim@havan.com.br>
2021-10-15 16:40:05 -0300

    Adding Health Check for Catalog.API Microservices with Checking MongoDb Connect

--- src/Services/Catalog/Catalog.API/Startup.cs
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -1,10 +1,13 @@
 using Catalog.API.Data;
 using Catalog.API.Repositors;
 using Commom.Logging;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
@@ -31,6 +34,11 @@ namespace Catalog.API
             });
             services.AddScoped<ICatalogContext, CatalogContext>();
             services.AddScoped<IProductRepository, ProductRepository>();
+
+            services.AddHealthChecks()
+                    .AddMongoDb(Configuration["DatabaseSettings:ConnectionString"], 
+                                "Catalog MongoDB Health Checks", 
+                                 HealthStatus.Degraded);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +58,12 @@ namespace Catalog.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                //context to healthChecks
+                endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
             });
         }
     }
