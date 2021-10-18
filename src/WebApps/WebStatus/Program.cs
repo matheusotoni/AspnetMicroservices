629d5748c8eaa7b671236c8b8aed6b1d1e9511fd
Matheus Otoni Amorim Silva <matheus.amorim@havan.com.br>
2021-10-18 14:22:39 -0300

    Developing WebStatus App for Centralized Microservices Health Monitoring Part 2

--- src/WebApps/WebStatus/Program.cs
+++ src/WebApps/WebStatus/Program.cs
@@ -1,11 +1,7 @@
+using Commom.Logging;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Serilog;
 
 namespace WebStatus
 {
@@ -18,6 +14,7 @@ namespace WebStatus
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog(SeriLogger.Configure)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
